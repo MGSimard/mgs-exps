2e1564c55e7513ee7b050833b9a873bcec0ed2b4
MGSimard <mgsimard.dev@gmail.com>
2026-03-23 17:26:22 -0400

    just move em here I guess?

--- src/components/GridGallery.tsx
+++ src/components/GridGallery.tsx
@@ -20,18 +20,19 @@ export function GridGallery({ slides, className }: GridGalleryProps) {
 
   return (
     <>
-      <div
-        className={cn(
-          "grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-2 [&>button]:block [&>button]:w-full [&>button]:overflow-hidden [&>button:focus-visible>img]:scale-[1.03] [&>button:focus-visible>img]:contrast-[0.9] [&>button:focus-visible>img]:saturate-[0.7] [&>button:hover>img]:scale-[1.03] [&>button:hover>img]:contrast-[0.9] [&>button:hover>img]:saturate-[0.7] [&>button>img]:block [&>button>img]:w-full [&>button>img]:border [&>button>img]:transition-all [&>button>img]:duration-50",
-          className
-        )}>
+      <div className={cn("grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-2", className)}>
         {slides.map((slide, index) => (
           <button
             key={slide.url}
             type="button"
             onClick={() => openLightbox(index)}
-            className="cursor-pointer bg-transparent">
-            <img src={slide.url} alt={slide.alt} loading="lazy" />
+            className="group aspect-video cursor-pointer overflow-hidden border bg-transparent">
+            <img
+              src={slide.url}
+              alt={slide.alt}
+              loading="lazy"
+              className="h-full w-full object-cover transition-all duration-50 group-hover:scale-[1.03] group-hover:contrast-[0.9] group-hover:saturate-[0.7] group-focus-visible:scale-[1.03] group-focus-visible:contrast-[0.9] group-focus-visible:saturate-[0.7]"
+            />
           </button>
         ))}
       </div>
