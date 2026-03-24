0ba3e820e3e9e01351e463270b2c9f89a6c06d4d
MGSimard <mgsimard.dev@gmail.com>
2026-03-24 14:16:13 -0400

    wee

--- src/routes/utility/ui-libraries.tsx
+++ src/routes/utility/ui-libraries.tsx
@@ -1,15 +1,19 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { PageTypeCSV } from "@/components/layout/PageType-CSV";
-import { Section } from "@/components/elements/Section";
 
 export const Route = createFileRoute("/utility/ui-libraries")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return (
-    <PageTypeCSV label="UTILITY" title="UI LIBRARIES">
-      <Section></Section>
-    </PageTypeCSV>
-  );
+  return <PageTypeCSV label="UTILITY" title="UI LIBRARIES" data={TABLE_DATA} />;
 }
+
+const TABLE_DATA = [
+  {
+    id: "lib-001",
+    name: "Item",
+    url: "https://www.url.com",
+    tags: ["tag", "example"],
+  },
+];
